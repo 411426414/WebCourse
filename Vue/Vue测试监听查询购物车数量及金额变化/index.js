6c4c25ee849775e9fb96a2d6dcd915143a88608e
郑杰 <411426414@qq.com>
2020-11-13 23:22:52 +0800

    添加基本数据

--- Vue/Vue测试监听查询购物车数量及金额变化/index.js
+++ Vue/Vue测试监听查询购物车数量及金额变化/index.js
@@ -4,14 +4,14 @@ window.onload = function () {
     data: {
       input: '',
       items: [
-          {name:'苹果', price:10, count :10, url:'./images/苹果.jpg'},
-          {name:'生匣子', price:99.9, count:1, url:'./images/年匣子.jpg'},
-          {name:'火龙果', price:23 , count:1, url:'./images/火龙果.jpg'},
-          {name:'百香果', price:9.4, count:2, url:'./images/百香果.jpg'},
-          {name:'哈密瓜', price:233, count:1, url:'./images/哈密瓜.jpg'},
-          {name: '橘子', price:2, count:1, url:'./images/橘子.jpg'},
-          {name:'桃子', price:34, count:1, url:'./images/桃子.jpg'},
-          {name:'西瓜', price:34, count:1, url:'./images/西瓜.jpg'}
+        {name:'苹果', price:10, count :10, url:'./images/苹果.jpg'},
+        {name:'生匣子', price:99.9, count:1, url:'./images/年匣子.jpg'},
+        {name:'火龙果', price:23 , count:1, url:'./images/火龙果.jpg'},
+        {name:'百香果', price:9.4, count:2, url:'./images/百香果.jpg'},
+        {name:'哈密瓜', price:233, count:1, url:'./images/哈密瓜.jpg'},
+        {name: '橘子', price:2, count:1, url:'./images/橘子.jpg'},
+        {name:'桃子', price:34, count:1, url:'./images/桃子.jpg'},
+        {name:'西瓜', price:34, count:1, url:'./images/西瓜.jpg'}
       ]
     },
     // 过滤器
