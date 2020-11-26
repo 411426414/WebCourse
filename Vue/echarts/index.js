8da988d31a93b175c2d1b411f3795a60a06a72ce
郑杰 <411426414@qq.com>
2020-11-26 23:17:02 +0800

    指定图表的配置项和数据

--- Vue/echarts/index.js
+++ Vue/echarts/index.js
@@ -50,52 +50,63 @@ window.onload = function () {
   myChart1.setOption(option1);
 
   // 动态获取数据
-  function getData(){
+  function getData() {
     var request = new XMLHttpRequest();
     request.open('get', 'get_center1')
   }
   getData()
 
-
   // 饼图
   option2 = {
     title: {
-        text: '某站点用户访问来源',
-        subtext: '纯属虚构',
-        left: 'center'
+      text: '某站点用户访问来源',
+      subtext: '纯属虚构',
+      left: 'center'
     },
     tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)'
     },
     legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      orient: 'vertical',
+      left: 'left',
+      data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
     },
-    series: [
+    series: [{
+      // name: '访问来源',
+      type: 'pie',
+      radius: '55%',
+      center: ['50%', '60%'],
+      data: [{
+          value: 335,
+          name: '直接访问'
+        },
         {
-            // name: '访问来源',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '60%'],
-            data: [
-                {value: 335, name: '直接访问'},
-                {value: 310, name: '邮件营销'},
-                {value: 234, name: '联盟广告'},
-                {value: 135, name: '视频广告'},
-                {value: 1548, name: '搜索引擎'}
-            ],
-            emphasis: {
-                itemStyle: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
+          value: 310,
+          name: '邮件营销'
+        },
+        {
+          value: 234,
+          name: '联盟广告'
+        },
+        {
+          value: 135,
+          name: '视频广告'
+        },
+        {
+          value: 1548,
+          name: '搜索引擎'
         }
-    ]
-};
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }]
+  };
   // 基于准备好的dom，初始化echarts实例
   var myChart2 = echarts.init(document.getElementById('echarts2'));
   // 使用刚指定的配置项和数据显示图表。
